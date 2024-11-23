e12dc44c8737c6614b9e5ade9608c5aa61ca30b6
Andrew <andrewspurlock@rocketmail.com>
2024-11-23 12:31:21 -0600

    Unit Tests created and successfully passed

--- Sportsplex.Tests/CallTests/BookingTests.cs
+++ Sportsplex.Tests/CallTests/BookingTests.cs
@@ -1,12 +1,179 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using Sportsplex.DTO;
+using Sportsplex.Interfaces;
+using Sportsplex.Models;
+using Sportsplex.Services;
 
 namespace Sportsplex.Tests.CallTests
 {
-    internal class BookingTests
+    public class BookingTests
     {
+
+        private readonly Mock<IBookingRepository> _mockBookingRepository;
+        private readonly IBookingService _BookingService;
+
+        public BookingTests()
+        {
+            _mockBookingRepository = new Mock<IBookingRepository>();
+            _BookingService = new BookingService(_mockBookingRepository.Object);
+        }
+
+
+        [Fact]
+        public async Task GetBookingsAsync_WhenCalled_ReturnBookingsAsync()
+        {
+            var Bookings = new List<Booking>
+            {
+                new Booking {Id = 1 },
+                new Booking {Id = 2 },
+                new Booking {Id = 3 }
+            };
+
+            _mockBookingRepository.Setup(x => x.GetAllBookingsAsync()).ReturnsAsync(Bookings);
+
+            var result = await _BookingService.GetAllBookingsAsync();
+
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Count);
+        }
+
+        [Fact]
+        public async Task CreateBookingAsync_WhenCalled_ReturnNewBookingAsync()
+        {
+
+            var BookingDTO = new CreateBookingDTO
+            {
+
+                OwnerId = 1,
+                Image = "string",
+                Description = "more string",
+                Rsvps = 7,
+                ReservedDate = DateTime.Now,
+                CategoryId = 2
+
+            };
+
+            var Booking = new Booking
+            {
+                OwnerId = BookingDTO.OwnerId,
+                Image = BookingDTO.Image,
+                Description = BookingDTO.Description,
+                Rsvps = BookingDTO.Rsvps,
+                ReservedDate = BookingDTO.ReservedDate,
+                CategoryId = BookingDTO.CategoryId,
+
+            };
+
+            _mockBookingRepository.Setup(x => x.CreateBookingAsync(BookingDTO)).ReturnsAsync(Booking);
+
+            var result = await _BookingService.CreateBookingAsync(BookingDTO);
+
+            Assert.NotNull(result);
+            Assert.Equal(BookingDTO.OwnerId, result.OwnerId);
+            Assert.Equal(BookingDTO.Image, result.Image);
+            Assert.Equal(BookingDTO.Description, result.Description);
+            Assert.Equal(BookingDTO.Rsvps, result.Rsvps);
+            Assert.Equal(BookingDTO.ReservedDate, result.ReservedDate);
+            Assert.Equal(BookingDTO.CategoryId, result.CategoryId);
+
+        }
+
+        [Fact]
+        public async Task UpdateBookingAsync_WhenCalled_ReturnUpdateBookingAsync()
+        {
+
+            int Bookingid = 1;
+
+            var Booking = new Booking
+            {
+                OwnerId = Bookingid,
+                Image = "original string",
+                Description = "more string",
+                Rsvps = 7,
+                ReservedDate = DateTime.Now,
+                CategoryId = 2
+            };
+
+            var editBookingDTO = new UpdateBookingDTO
+            {
+                Image = "not string",
+                Description = "less string",
+                ReservedDate = DateTime.Now,
+                CategoryId = 3
+            };
+
+            var updatedBooking = new Booking
+            {
+                Image = editBookingDTO.Image,
+                Description = editBookingDTO.Description,
+                ReservedDate = editBookingDTO.ReservedDate,
+                CategoryId = editBookingDTO.CategoryId
+            };
+
+            _mockBookingRepository.Setup(x => x.GetBookingByIdAsync(Bookingid)).ReturnsAsync(Booking);
+            _mockBookingRepository.Setup(x => x.UpdateBookingAsync(Bookingid, editBookingDTO)).ReturnsAsync(updatedBooking);
+
+            var result = await _BookingService.UpdateBookingAsync(Bookingid, editBookingDTO);
+
+            Assert.NotNull(result);
+            Assert.Equal(editBookingDTO.Image, result.Image);
+            Assert.Equal(editBookingDTO.Description, result.Description);
+            Assert.Equal(editBookingDTO.ReservedDate, result.ReservedDate);
+            Assert.Equal(editBookingDTO.CategoryId, result.CategoryId);
+
+
+        }
+
+        [Fact]
+        public async Task DeleteBookingAsync_WhenCalled_ReturnDeletedBookingAsync()
+        {
+
+            int BookingId = 1;
+
+            var Booking = new Booking
+            {
+                Id = BookingId
+            };
+
+            _mockBookingRepository.Setup(x => x.GetBookingByIdAsync(BookingId)).ReturnsAsync(Booking);
+
+            await _BookingService.DeleteBookingAsync(BookingId);
+
+            _mockBookingRepository.Verify(x => x.DeleteBookingAsync(BookingId), Times.Once);
+
+            _mockBookingRepository.Setup(x => x.GetBookingByIdAsync(BookingId)).ReturnsAsync((Booking)null);
+
+        }
+
+        [Fact]
+        public async Task GetSingleBookingAsync_WhenCalled_ReturnsBooking()
+        {
+            int BookingId = 1;
+            var Booking = new Booking
+            {
+                Id = 1,
+                OwnerId = 2,
+                Image = "image",
+                Description = "Description",
+                Rsvps = 10,
+                ReservedDate = DateTime.Now,
+                CategoryId = 2
+            };
+
+            // Setup the repository to return the expected Booking when requested
+            _mockBookingRepository.Setup(x => x.GetBookingByIdAsync(BookingId)).ReturnsAsync(Booking);
+
+            // Act
+            var result = await _BookingService.GetBookingByIdAsync(BookingId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(Booking.Id, result.Id);
+            Assert.Equal(Booking.Description, result.Description);
+            Assert.Equal(Booking.Image, result.Image);
+            Assert.Equal(Booking.Rsvps, result.Rsvps);
+            Assert.Equal(Booking.ReservedDate, result.ReservedDate);
+            Assert.Equal(Booking.CategoryId, result.CategoryId);
+        }
     }
 }
--- Sportsplex.Tests/CallTests/LocationTests.cs
+++ Sportsplex.Tests/CallTests/LocationTests.cs
@@ -1,12 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
+using Sportsplex.DTO;
+using Sportsplex.Interfaces;
+using Sportsplex.Models;
+using Sportsplex.Services;
+
 
 namespace Sportsplex.Tests.CallTests
 {
-    internal class LocationTests
+    public class LocationTests
     {
+        private readonly Mock<ILocationRepository> _mockLocationRepository;
+        private readonly ILocationService _LocationService;
+
+        public LocationTests()
+        {
+            _mockLocationRepository = new Mock<ILocationRepository>();
+            _LocationService = new LocationService(_mockLocationRepository.Object);
+        }
+
+
+        [Fact]
+        public async Task GetLocationsAsync_WhenCalled_ReturnLocationsAsync()
+        {
+            var Locations = new List<Location>
+            {
+                new Location {Id = 1 },
+                new Location {Id = 2 },
+                new Location {Id = 3 }
+            };
+
+            _mockLocationRepository.Setup(x => x.GetAllLocationsAsync()).ReturnsAsync(Locations);
+
+            var result = await _LocationService.GetAllLocationsAsync();
+
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Count);
+        }
+
+        [Fact]
+        public async Task CreateLocationAsync_WhenCalled_ReturnNewLocationAsync()
+        {
+
+            var LocationDTO = new CreateLocationDTO
+            {
+                Name = "Lebanon"
+            };
+
+            var Location = new Location
+            {
+                Name = LocationDTO.Name
+            };
+
+            _mockLocationRepository.Setup(x => x.CreateLocationAsync(LocationDTO)).ReturnsAsync(Location);
+
+            var result = await _LocationService.CreateLocationAsync(LocationDTO);
+
+            Assert.NotNull(result);
+            Assert.Equal(LocationDTO.Name, result.Name);
+        }
+
+        [Fact]
+        public async Task UpdateLocationAsync_WhenCalled_ReturnUpdateLocationAsync()
+        {
+
+            int LocationId = 1;
+
+            var Location = new Location
+            {
+                Name = "Madison"
+            };
+
+            var editLocationDTO = new UpdateLocationDTO
+            {
+                Name = "Goodlettsville"
+            };
+
+            var updatedLocation = new Location
+            {
+                Name = editLocationDTO.Name
+            };
+
+            _mockLocationRepository.Setup(x => x.GetLocationByIdAsync(LocationId)).ReturnsAsync(Location);
+            _mockLocationRepository.Setup(x => x.UpdateLocationAsync(LocationId, editLocationDTO)).ReturnsAsync(updatedLocation);
+
+            var result = await _LocationService.UpdateLocationAsync(LocationId, editLocationDTO);
+
+            Assert.NotNull(result);
+            Assert.Equal(editLocationDTO.Name, result.Name);
+        }
+
+        [Fact]
+        public async Task GetLocationByIdAsync_WhenCalled_ReturnLocationByIdAsync()
+        {
+
+            var Location = new Location
+            {
+                Id = 1,
+                Name = "Bowling Green"
+            };
+
+            _mockLocationRepository.Setup(x => x.GetLocationByIdAsync(Location.Id)).ReturnsAsync(Location);
+
+            var result = await _LocationService.GetLocationByIdAsync(Location.Id);
+
+            Assert.NotNull(result);
+            Assert.Equal(Location.Id, result.Id);
+            Assert.Equal(Location.Name, result.Name);
+        }
+
+        [Fact]
+        public async Task DeleteLocationAsync_WhenCalled_ReturnDeletedLocationAsync()
+        {
+
+            var Location = new Location
+            {
+                Id = 1,
+                Name = "Bowling Green"
+            };
+
+            _mockLocationRepository.Setup(x => x.GetLocationByIdAsync(Location.Id)).ReturnsAsync(Location);
+
+            await _LocationService.DeleteLocationAsync(Location.Id);
+
+            _mockLocationRepository.Verify(x => x.DeleteLocationAsync(Location.Id), Times.Once);
+
+            _mockLocationRepository.Setup(x => x.GetLocationByIdAsync(Location.Id)).ReturnsAsync((Location)null);
+        }
     }
 }
